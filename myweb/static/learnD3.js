17c2d106a94535096994646ccf76c6aa634158b6
outman66912 <15951923234@163.com>
2017-05-21 20:23:18 +0800

    hello

--- myweb/static/learnD3.js
+++ myweb/static/learnD3.js
@@ -9,11 +9,53 @@ var xAxisWidth=300;
 var yAxisWidth=300;
 
 var center=[[0.5,0.5],[0.7,0.8],[0.4,0.9],[0.11,0.32],[0.88,0.25],[0.75,0.12],[0.5,0.1],[0.2,0.3],[0.4,0.1],[0.6,0.7]];
-//draw();
-//draw2();
-//scale();
-//scale2()
+
+var dataset2=[{country:"china",gdp:[[2000,11920],[2001,13170],[2002,14550],[2003,16500],[2004,19440],[2005,22870],[2006,27930]]},
+              {country:"japan",gdp:[[2000,47310],[2001,41590],[2002,39800],[2003,43020],[2004,46550],[2005,45710],[2006,43560]]}];
+
 eventBinding();
+draw3();
+function scale(){
+    var svg=d3.select("body").append("svg")
+              .attr("width",width)
+              .attr("height",height);
+    var xScale=d3.scaleLinear()
+                 .domain([0,10])
+                 .range([0,300]);
+    var axis = d3.axisBottom()
+                .scale(xScale);
+
+    var axis1=d3.axisLeft().scale(xScale).ticks(5).tickSize(6)
+    .tickSizeInner(10).tickSizeOuter(88).tickFormat(d3.format("$0.1f"));
+    var axis2=d3.axisLeft().scale(xScale).tickValues([3,4,5,6]);
+    var axis3=d3.axisLeft().scale(xScale).tickArguments([10, "s"]);
+    var gAxis=svg.append("g").attr("transform","translate(80,80)");
+//    axis(gAxis);
+    axis1(gAxis);
+//    axis2(gAxis);
+//    axis3(gAxis);
+}
+function scale2(){
+    var svg=d3.select("body").append("svg")
+              .attr("width",width)
+              .attr("height",height);
+
+    var xScale=d3.scaleLinear()
+                 .domain([0,10])
+                 .rangeRound([0,300]);
+    var xScale2=d3.scalePow().exponent(2)
+                 .domain([0,10])
+                 .rangeRound([0,310]);
+
+    var xScale3=d3.scaleTime()
+                 .domain([new Date(2000, 0, 1, 0), new Date(2000, 0,1, 5)])
+                 .rangeRound([0,300]);
+    var axis=d3.axisBottom().scale(xScale3).ticks(d3.timeMinute.every(60));
+
+    var gAxis=svg.append("g").attr("transform","translate(80,80)");
+    gAxis.call(axis);
+}
+
 function draw(){
     d3.select("svg").remove();
     var svg=d3.select("body").append("svg").attr("width",width).attr("height",height);
@@ -147,49 +189,56 @@ function draw2(){
 
 
 }
-
-
-function scale(){
-    var svg=d3.select("body").append("svg")
-              .attr("width",width)
-              .attr("height",height);
-    var xScale=d3.scaleLinear()
-                 .domain([0,10])
-                 .range([0,300]);
-    var axis = d3.axisBottom()
-                .scale(xScale);
-
-    var axis1=d3.axisLeft().scale(xScale).ticks(5).tickSize(6)
-    .tickSizeInner(10).tickSizeOuter(88).tickFormat(d3.format("$0.1f"));
-    var axis2=d3.axisLeft().scale(xScale).tickValues([3,4,5,6]);
-    var axis3=d3.axisLeft().scale(xScale).tickArguments([10, "s"]);
-    var gAxis=svg.append("g").attr("transform","translate(80,80)");
-//    axis(gAxis);
-    axis1(gAxis);
-//    axis2(gAxis);
-//    axis3(gAxis);
-}
-
-function scale2(){
-    var svg=d3.select("body").append("svg")
-              .attr("width",width)
-              .attr("height",height);
+function draw3(){
+    var maxGdp=0;
+    for(var i=0;i<dataset2.length;i++){
+    var temp=d3.max(dataset2[i].gdp,function(d){return d[1]});
+    if(temp>maxGdp){
+        maxGdp=temp;
+    }
+    }
 
     var xScale=d3.scaleLinear()
-                 .domain([0,10])
-                 .rangeRound([0,300]);
-    var xScale2=d3.scalePow().exponent(2)
-                 .domain([0,10])
-                 .rangeRound([0,310]);
+                .domain([2000,2005])
+                .range([0,width-padding.left-padding.right]);
+    var yScale=d3.scaleLinear()
+                .domain([0,maxGdp])
+                .range([height-padding.top-padding.bottom,0]);
 
-    var xScale3=d3.scaleTime()
-                 .domain([new Date(2000, 0, 1, 0), new Date(2000, 0,1, 5)])
-                 .rangeRound([0,300]);
-    var axis=d3.axisBottom().scale(xScale3).ticks(d3.timeMinute.every(60));
+    var linePath=d3.line()
+                   .x(function(d){return xScale(d[0]);})
+                   .y(function(d){return yScale(d[1]);})
+                   .curve(d3.curveBasis);
 
-    var gAxis=svg.append("g").attr("transform","translate(80,80)");
-    gAxis.call(axis);
+
+    var colors=[d3.rgb(0,0,255),d3.rgb(0,255,0)];
+    var svg=d3.select("body").append("svg").attr("width",width).attr("height",height);
+    var updatePath=svg.selectAll(".path").data(dataset2);
+    var enterPath=updatePath.enter();
+    var exitPath=updatePath.exit();
+    enterPath.append("path")
+             .attr("transform","translate("+padding.left+","+padding.top+")")
+             .attr("d",function(d){return linePath(d.gdp)})
+             .attr("fill","none")
+             .attr("stroke-width",3)
+             .attr("stroke",function(d,i){
+                return colors[i];
+             });
+    updatePath.attr("transform","translate("+padding.left+","+padding.top+")")
+             .attr("d",function(d){return linePath(d.gdp)})
+             .attr("fill","none")
+             .attr("stroke-width",3)
+             .attr("stroke",function(d,i){
+                return colors[i];
+             });
+    exitPath.remove();
+    var xAxis = d3.axisBottom().scale(xScale).tickSizeOuter(0).ticks(5).tickFormat(d3.format("d"));
+    var yAxis=d3.axisLeft().scale(yScale).tickSizeOuter(0);
+    svg.append("g").attr("class","axis").attr("transform","translate("+padding.left+","+(height-padding.bottom)+")").call(xAxis);
+    svg.append("g").attr("class","axis").attr("transform","translate("+padding.left+","+(padding.top)+")").call(yAxis);
 }
+
+
 
 function eventBinding(){
 $("#sort").on("click",function(){
