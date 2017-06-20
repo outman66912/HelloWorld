f9d85fa4079981ee847813e4d0763cf6319368f0
outman66912 <15951923234@163.com>
2017-06-20 17:31:50 +0800

    hello

--- myweb/static/learnD3.js
+++ myweb/static/learnD3.js
@@ -97,7 +97,68 @@ var timeText;
 eventBinding();
 var svg=d3.select("body").append("svg").attr("width",width).attr("height",height);
 var path=svg.selectAll("path").append(path).attr("d",d3.path().lineTo(3, 4));
-topo()
+voronoi()
+function voronoi(){
+d3.selectAll("svg").remove();
+ var svg=d3.select("body").append("svg").attr("width",width).attr("height",height);
+var  radius = 32;
+var circles = d3.range(20).map(function() {
+  return {
+    x: Math.round(Math.random() * (width - radius * 2) + radius),
+    y: Math.round(Math.random() * (height - radius * 2) + radius)
+  };
+});
+var color = d3.scaleOrdinal()
+    .range(d3.schemeCategory20);
+
+var voronoi = d3.voronoi()
+    .x(function(d) { return d.x; })
+    .y(function(d) { return d.y; })
+    .extent([[-1, -1], [width + 1, height + 1]]);
+var circle = svg.selectAll("g")
+  .data(circles)
+  .enter().append("g")
+    .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+
+        var cell = circle.append("path")
+  .data(voronoi.polygons(circles))
+    .attr("d", renderCell)
+    .attr("id", function(d, i) { return "cell-" + i; });
+
+    circle.append("clipPath")
+    .attr("id", function(d, i) { return "clip-" + i; })
+  .append("use")
+    .attr("xlink:href", function(d, i) { return "#cell-" + i; });
+
+    circle.append("circle")
+    .attr("clip-path", function(d, i) { return "url(#clip-" + i + ")"; })
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .attr("r", radius)
+    .style("fill", function(d, i) { return color(i); });
+
+function dragstarted(d) {
+  d3.select(this).raise().classed("active", true);
+}
+
+function dragged(d) {
+  d3.select(this).select("circle").attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+  cell = cell.data(voronoi.polygons(circles)).attr("d", renderCell);
+}
+
+function dragended(d, i) {
+  d3.select(this).classed("active", false);
+}
+
+function renderCell(d) {
+  return d == null ? null : "M" + d.join("L") + "Z";
+}
+
+
+}
 function topo(){
 d3.selectAll("svg").remove();
  var svg=d3.select("body").append("svg").attr("width",width).attr("height",height);
@@ -1527,6 +1588,9 @@ $("#example16").on("click",function(){
     hideButton();
     brush();
 });
-
+$("#example17").on("click",function(){
+    hideButton();
+    voronoi();
+});
 }
 })
